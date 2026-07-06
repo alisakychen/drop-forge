90587d433fd1560c4dbba2abe2f17599738539cf
Alisa Chen <alisa.ky.chen.code@gmail.com>
2026-07-05 20:28:44 -0400

    force restart

--- src/main/java/com/celnoda/dropforge/dao/GameDAO.java
+++
@@ -1,9 +0,0 @@
-package com.celnoda.dropforge.dao;
-
-import java.util.List;
-
-import com.celnoda.dropforge.model.Game;
-
-public interface GameDAO {
-	public List<Game> getGames();
-}
--- src/main/java/com/celnoda/dropforge/service/GameManager.java
+++
@@ -1,9 +0,0 @@
-package com.celnoda.dropforge.service;
-
-import java.util.List;
-
-import com.celnoda.dropforge.model.*;
-
-public interface GameManager {
-	public List<Game> getGames();
-}
